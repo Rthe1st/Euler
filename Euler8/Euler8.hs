e1615b4299bef9078f9c26e2d1a1666d813d533a
Mehow <Kaiser.mehow@gmail.com>
2015-03-02 14:02:04 +0000

    Searchstring is now taken from a file file, needle length and algorithm can be speified using commandline args

--- Euler8/Euler8.hs
+++ Euler8/Euler8.hs
@@ -1,39 +1,44 @@
 import Data.Char
+import System.IO
+import Data.List
+import System.Environment
 
-searchString = "73167176531330624919225119674426574742355349194934\
-\96983520312774506326239578318016984801869478851843\
-\85861560789112949495459501737958331952853208805511\
-\12540698747158523863050715693290963295227443043557\
-\66896648950445244523161731856403098711121722383113\
-\62229893423380308135336276614282806444486645238749\
-\30358907296290491560440772390713810515859307960866\
-\70172427121883998797908792274921901699720888093776\
-\65727333001053367881220235421809751254540594752243\
-\52584907711670556013604839586446706324415722155397\
-\53697817977846174064955149290862569321978468622482\
-\83972241375657056057490261407972968652414535100474\
-\82166370484403199890008895243450658541227588666881\
-\16427171479924442928230863465674813919123162824586\
-\17866458359124566529476545682848912883142607690042\
-\24219022671055626321111109370544217506941658960408\
-\07198403850962455444362981230987879927244284909188\
-\84580156166097919133875499200524063689912560717606\
-\05886116467109405077541002256983155200055935729725\
-\71636269561882670428252483600823257530420752963450"
+main = do
+    args <- getArgs
+    case args of
+        [searchFile,needleLength,"maxima"] ->
+            runMaxima searchFile needleLength
+        [searchFile,needleLength,"naive"] ->
+            runNaive searchFile needleLength
+        [searchFile,needleLength] ->
+            runNaive searchFile needleLength
+        _ -> putStrLn "Requires arguments: fileName needleLength [algorithm]\n where algorithm must be one of: naive, maxima. Defaults to naive"
 
-searchStringAsIntList = (map digitToInt searchString)
-needleLength = 13
+runMaxima :: String -> String -> IO()
+runMaxima searchFile needleLength = do
+    contents <- readFile searchFile
+    let intList = map digitToInt contents
+    output (findLargestMaxima intList (read needleLength))
+
+runNaive :: String -> String -> IO()
+runNaive searchFile needleLength = do
+    contents <- readFile searchFile
+    let intList = map digitToInt contents
+    output (findMaxProductNaive intList (read needleLength) 0)
+
+output :: Int -> IO()
+output maxProduct = do
+    putStrLn "Max product was :"
+    print maxProduct
 
 {-
 Basic algorithm, literally move through the list, evaluating the product of the first n elements
 If that product is larger then the previous largest, remember it.
 -}
-fakeMain1 = findMaxProductNaive searchStringAsIntList 0
-
-findMaxProductNaive :: [Int] -> Int -> Int
-findMaxProductNaive hayStack maxProduct
+findMaxProductNaive :: [Int] -> Int -> Int -> Int
+findMaxProductNaive hayStack needleLength maxProduct
 	| length hayStack < needleLength = maxProduct
-	| otherwise = findMaxProductNaive (tail hayStack) (max currentProduct maxProduct)
+	| otherwise = findMaxProductNaive (tail hayStack) needleLength (max currentProduct maxProduct)
 	where currentProduct = product (take needleLength hayStack)
 
 	{-
@@ -54,25 +59,23 @@ findMaxProductNaive hayStack maxProduct
 	only one > or < comparison is being done,
 	verses n multiply operations in the naive method
 	-}
-	
-fakeMain2 = findLargestMaxima (map digitToInt searchString)
 
-findLargestMaxima :: [Int] -> Int
-findLargestMaxima digitList = case digitList of
+findLargestMaxima :: [Int] -> Int -> Int
+findLargestMaxima digitList needleLength = case digitList of
 	[] -> 0
-	_ -> max maxima (findLargestMaxima afterMaxima)
-	where remaingDigitList = findFirstMaximaPosition (findFirstMinimaPosition digitList)
+	_ -> max maxima (findLargestMaxima afterMaxima needleLength)
+	where remaingDigitList = findFirstMaximaPosition (findFirstMinimaPosition digitList needleLength) needleLength
    	      afterMaxima = drop needleLength remaingDigitList
    	      maxima = product (take needleLength remaingDigitList)
 
-findFirstMinimaPosition :: [Int] -> [Int]
-findFirstMinimaPosition digitList
+findFirstMinimaPosition :: [Int] -> Int -> [Int]
+findFirstMinimaPosition digitList needleLength
 	| length digitList == needleLength+1 = digitList
 	| head digitList < digitList!!needleLength = digitList
-	| head digitList >= digitList!!needleLength = findFirstMinimaPosition (tail digitList)
+	| head digitList >= digitList!!needleLength = findFirstMinimaPosition (tail digitList) needleLength
 
-findFirstMaximaPosition :: [Int] -> [Int]
-findFirstMaximaPosition digitList
+findFirstMaximaPosition :: [Int] -> Int -> [Int]
+findFirstMaximaPosition digitList needleLength
 	| length digitList == needleLength+1 = tail digitList
 	| head digitList > digitList!!needleLength = digitList
-	| head digitList <= digitList!!needleLength = findFirstMaximaPosition (tail digitList)
+	| head digitList <= digitList!!needleLength = findFirstMaximaPosition (tail digitList) needleLength
