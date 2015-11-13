e7b3ebcaf7936a9615a4be9d025a16fe39fb4546
Mehow <Kaiser.mehow@gmail.com>
2015-11-13 21:17:14 +0000

    indentation change

--- Euler8/Euler8.hs
+++ Euler8/Euler8.hs
@@ -41,24 +41,24 @@ findMaxProductNaive hayStack needleLength maxProduct
 	| otherwise = findMaxProductNaive (tail hayStack) needleLength (max currentProduct maxProduct)
 	where currentProduct = product (take needleLength hayStack)
 
-	{-
-	More complex
-	At a given point in the string let the current "needle" be n, n+1... n+m and have product p(n, n+m)
-	If the next digit in the string (n+m+1) is less then n, then p(n+1,n+m+1) will be less then p(n, n+m)
-	By induction then all following products will be less then the original p(n, n+m) until a n+m+1 > n.
-	This can be seen as a local minima.
-	The reverse applies for finding maximas, if n+m+1 > n, then p(n+1, n+m+1) > p(n, n+m)
-	Thus, from the start
-	1) Evaluate the next digit (n+m+1) against n until a local minima is reached
-	2) Then repeat until a maxima is reached.
-	3) Then remember this local maxima and repeat
-	4) Repeat 1, 2 and 3 until end of search string
-	Eventually all local maximas will be found, at which point the largest can be selected as the answer
-	
-	Haven't profiled, but should (maybe...) be faster then naive method because for each digit advanced in the search string
-	only one > or < comparison is being done,
-	verses n multiply operations in the naive method
-	-}
+{-
+More complex
+At a given point in the string let the current "needle" be n, n+1... n+m and have product p(n, n+m)
+If the next digit in the string (n+m+1) is less then n, then p(n+1,n+m+1) will be less then p(n, n+m)
+By induction then all following products will be less then the original p(n, n+m) until a n+m+1 > n.
+This can be seen as a local minima.
+The reverse applies for finding maximas, if n+m+1 > n, then p(n+1, n+m+1) > p(n, n+m)
+Thus, from the start
+1) Evaluate the next digit (n+m+1) against n until a local minima is reached
+2) Then repeat until a maxima is reached.
+3) Then remember this local maxima and repeat
+4) Repeat 1, 2 and 3 until end of search string
+Eventually all local maximas will be found, at which point the largest can be selected as the answer
+
+Haven't profiled, but should (maybe...) be faster then naive method because for each digit advanced in the search string
+only one > or < comparison is being done,
+verses n multiply operations in the naive method
+-}
 
 findLargestMaxima :: [Int] -> Int -> Int
 findLargestMaxima digitList needleLength = case digitList of
